4e3e1bafca50e9b2da318c3ce82438efd9872d51
Mick Schroeder <mschroeder@gmail.com>
2020-11-18 17:26:59 -0500

    update

--- src/js/components/zbib.jsx
+++ src/js/components/zbib.jsx
@@ -75,7 +75,7 @@ class ZBib extends React.PureComponent {
 									<AdSense.Google
 									client='ca-pub-6344797609391119'
 									slot='3922555336'
-									style={{ display: 'block', height: 100 }}
+									style={{ display: 'block' }}
 									format='auto'
 									responsive='true'
 									className='advertisement'
@@ -101,7 +101,7 @@ class ZBib extends React.PureComponent {
 									<AdSense.Google
 									client='ca-pub-6344797609391119'
 									slot='3922555336'
-									style={{ display: 'block', height: 100 }}
+									style={{ display: 'block' }}
 									format='auto'
 									responsive='true'
 									className='advertisement'
@@ -152,7 +152,7 @@ class ZBib extends React.PureComponent {
 									<AdSense.Google
 									client='ca-pub-6344797609391119'
 									slot='3922555336'
-									style={{ display: 'block', height: 100 }}
+									style={{ display: 'block' }}
 									format='auto'
 									responsive='true'
 									className='advertisement'
